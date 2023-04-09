ee8ab9d5618582846c23e3ddca77ac6516ab76be
Uladzislau Blok <blokv75@gmail.com>
2023-04-09 23:09:15 +0200

    Add UserRepository. Started making JwtToken processing

---
+++ src/main/java/com/Company/AccountService/config/JwtService.java
@@ -0,0 +1,33 @@
+package com.Company.AccountService.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+
+    private final static String SECRET_KEY = "3777217A24432646294A404E635266556A586E3272357538782F413F442A472D";
+
+    public String extractUserEmail(String jwtToken) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(jwtToken)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyByte = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyByte);
+    }
+}

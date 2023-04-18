334dadf0d8d81a9928752780db352c6ead4d5c98
Uladzislau Blok <blokv75@gmail.com>
2023-04-18 23:12:06 +0200

    I added password and email validation. Added password change request processing

--- src/main/java/com/Company/AccountService/auth/AuthenticationController.java
+++ src/main/java/com/Company/AccountService/auth/AuthenticationController.java
@@ -4,13 +4,16 @@ package com.Company.AccountService.auth;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
@@ -28,5 +31,14 @@ public class AuthenticationController {
             @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
+    }
+
+    @PostMapping("/changepass")
+    public ResponseEntity<Map<String, String>> changePassword(
+            @RequestBody ChangePassRequest request,
+            Authentication authentication
+    ) {
+        authenticationService.changePassword(request, authentication);
+        return ResponseEntity.ok(Map.of("status", "The password has been updated successfully"));
     }
 }
